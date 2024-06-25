7a6cc57754930a1063c76a8518440740ee921de5
Fatolu Stephen <smjssp@gmail.com>
2024-06-25 16:33:26 +0100

    Add comment

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from pytube import YouTube
 from sys import argv
+import pdb
+
+
 
 link = argv[1]
 yt = YouTube(link)
 
 print("Title:", yt.title)
 
+pdb.set_trace()
+
 print("Views", yt.views)
 
 yd = yt.streams.get_highest_resolution()
 
 yd.download(r"C:\Users\ndifreke\Desktop\Youtube")
+
